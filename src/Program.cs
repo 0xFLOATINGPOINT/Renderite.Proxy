a92e1c8bad6ad2e65c620699f7ac7dd6acf46113
Cloud_Jumper <creepergummy@protonmail.com>
2026-03-20 14:06:16 -0400

    Publish src

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string QueueName = args[1];
+        string QueueCapacity = args[3];
+        Console.WriteLine("----------------------------------------------------------------------------------------------------");
+        Console.WriteLine("Source code found at: https://github.com/0xFLOATINGPOINT/Renderite.Proxy");
+        Console.WriteLine("Licensed : MIT");
+        Console.WriteLine(string.Empty);
+        Console.WriteLine($"QueueName: {QueueName}");
+        Console.WriteLine($"QueueCapacity: {QueueCapacity}");
+        Process process = new Process();
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardInput = true;
+        process.StartInfo.FileName = "C:\\Windows\\System32\\clip.exe";
+        process.Start();
+        process.StandardInput.Write(QueueName);
+        process.StandardInput.Close();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
